0080a6178eb8b4dd8599901355ddb2f23b2efff6
Mohammed Younus <50220627+youniboy@users.noreply.github.com>
2022-11-29 01:52:58 +0530

    Update components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,38 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { ProductsingleComponent } from './productsingle/productsingle.component';
+import { CartComponent } from './cart/cart.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { ShopComponent } from './shop/shop.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { OrdersComponent } from './orders/orders.component';
+import { LoginComponent } from './login/login.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
+import { ProfileDetailsComponent } from './profile-details/profile-details.component';
+import { AddressComponent } from './address/address.component';
+import { EditAddressComponent } from './edit-address/edit-address.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    ProductsingleComponent,
+    CartComponent,
+    CheckoutComponent,
+    ShopComponent,
+    DashboardComponent,
+    OrdersComponent,
+    LoginComponent,
+    ForgotPasswordComponent,
+    ProfileDetailsComponent,
+    AddressComponent,
+    EditAddressComponent
   ],
   imports: [
     BrowserModule,
